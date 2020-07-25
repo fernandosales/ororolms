de426326264019b19507bfa3e97eccf13cc3c7b3
Fernando Sales <fernandosales@github.com>
2020-07-25 12:12:45 -0300

    Create the Course Bundle.

---
+++ src/CourseBundle/CourseBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CourseBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CourseBundle extends Bundle
+{
+}
---
+++ src/CourseBundle/DependencyInjection/CourseExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CourseBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+class CourseExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('form_types.yml');
+        $loader->load('services.yml');
+    }
+}
